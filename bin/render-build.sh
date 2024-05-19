0bab17ba50449517e95e62f4b32a8de5c991795b
MosDevx <mosesk.wamae@gmail.com>
2024-05-19 17:28:30 +0300

    remove sideqik for build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,7 +6,7 @@ bundle install
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec sidekiq 
+# bundle exec sidekiq 
 # bundle exec rails db:create RAILS_ENV=production  # Create the database
 # bundle exec rails db:migrate RAILS_ENV=production # Run migrations
 # bundle exec rails db:seed RAILS_ENV=production    # Seed the database (optional)
