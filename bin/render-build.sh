7581710210bef1c60fbedfaaa904037efe52c703
MosDevx <mosesk.wamae@gmail.com>
2024-05-19 17:03:27 +0300

    remove db set up; fix sidekiq;precompile options

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,6 @@ bundle install
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:create RAILS_ENV=production  # Create the database
-bundle exec rails db:migrate RAILS_ENV=production # Run migrations
-bundle exec rails db:seed RAILS_ENV=production    # Seed the database (optional)
+# bundle exec rails db:create RAILS_ENV=production  # Create the database
+# bundle exec rails db:migrate RAILS_ENV=production # Run migrations
+# bundle exec rails db:seed RAILS_ENV=production    # Seed the database (optional)
